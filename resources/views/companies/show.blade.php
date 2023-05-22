1bdc28bfcd57832cd9edecb07916b4746bb9c247
nakazawashion <nakazawa.shion@celsys.com>
2023-05-22 17:06:20 +0900

    viewの更新

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -1,16 +1,29 @@
-<h1>詳細画面</h1>
-<h2>{{ $company->name }}</h2>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Companies') }}
+        </h2>
+    </x-slot>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-<p>作成日時: {{ $company->created_at }}</p>
-<p>更新日時: {{ $company->updated_at }}</p>
+                {{ Html::linkRoute('companies.index', '一覧に戻る') }}
 
-<form action="/companies">
-    <button type="submit">戻る</button>
-</form>
+                {{ Html::linkRoute('companies.edit', '編集', compact('company')) }}
 
-<form method="POST" action="{{ route('companies.destroy', $company) }}">
-    @csrf
-    @method('DELETE')
-    <button type="submit">削除</button>
-</form>
+                <dl>
+                    <dt>ID</dt>
+                    <dd>{{ $company->id }}</dd>
+                    <dt>Name</dt>
+                    <dd>{{ $company->name }}</dd>
+                    <dt>Created at</dt>
+                    <dd>{{ $company->created_at }}</dd>
+                    <dt>Updated at</dt>
+                    <dd>{{ $company->updated_at }}</dd>
+                </dl>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
