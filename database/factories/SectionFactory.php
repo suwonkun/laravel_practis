9732ad1e3b8d3b5d49db31bf0475d96676ce4ba8
nakazawashion <nakazawa.shion@celsys.com>
2023-05-22 16:09:21 +0900

    sectionモデルの追加

---
+++ database/factories/SectionFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Company;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Section>
+ */
+class SectionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $company = Company::inRandomOrder()->first();
+
+        return [
+            'name' => 'エンジニア',
+            'company_id' => $company->id,
+        ];
+    }
+}
---
+++ database/migrations/2023_05_22_060743_create_sections_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Company::class)->comment('会社ID');
+            $table->string('name')->comment('部署名');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sections');
+    }
+};
