f74817a8fccf351a2f5b56c26cd39f7ff918868c
nakazawashion <nakazawa.shion@celsys.com>
2023-05-22 16:57:22 +0900

    中間テーブルの作成

---
+++ database/migrations/2023_05_22_072638_create_user_section_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_section', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Section::class)->comment('部署ID');
+            $table->foreignIdFor(\App\Models\User::class)->comment('ユーザーID');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_section');
+    }
+};
