b36384d063cb7b8d2972d606e0467af448eed5a8
nakazawashion <nakazawa.shion@celsys.com>
2023-05-25 16:40:53 +0900

    会社の認可

--- app/Policies/CompanyPolicy.php
+++ app/Policies/CompanyPolicy.php
@@ -13,82 +13,26 @@ class CompanyPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
+     * @param \App\Models\User $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function viewAny(User $user)
+    public function show(User $user, Company $company): bool
     {
-        //
+        return $user->company->id === $company->id;
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Company $company)
+    public function edit(User $user, Company $company): bool
     {
-        //
+        return $user->company->id === $company->id;
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function update(User $user, Company $company)
     {
-        //
+        return $user->company->id === $company->id;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function delete(User $user, Company $company)
     {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, Company $company)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Company  $company
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Company $company)
-    {
-        //
+        return $user->company->id === $company->id;
     }
 }
