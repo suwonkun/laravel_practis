9ce26f72558d6ecf28312f03b891f89d81fe9289
nakazawashion <nakazawa.shion@celsys.com>
2023-05-27 17:31:05 +0900

    中間テーブルの命名規則に従った変更

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -20,6 +20,6 @@ class Section extends Model
     }
     public function users()
     {
-        return $this->belongsToMany(User::class, 'user_sections', 'section_id', 'user_id');
+        return $this->belongsToMany(User::class, 'user_section', 'section_id', 'user_id');
     }
 }
